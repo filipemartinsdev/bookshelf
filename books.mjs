ff89dd29fcdc9087b238291ef405bc240b31ad9e
FilipeMartins-dev <filipe.martins.dev@gmail.com>
2024-12-12 16:03:54 -0300

    change book list info
    Add new descriptions to book list: Pusblisher and pages in the book

--- books.mjs
+++ books.mjs
@@ -3,14 +3,18 @@ export const booksList = [
         image: "./src/book-maisiedobbs.jpeg",
         title: "Maisie Dobbs",
         author: "Jackeline Wisnpear",
+        publisher:"Arqueiro",
+        pages: "285",
         status: "reading",
         rating: "-",
-        note: ""
+        note: "",
     },
     {
         image: "./src/book-aconvocacao.jpg",
         title: "A convocação",
         author: "Peadar O'Guilin",
+        publisher:"Melhoramentos",
+        pages: "271",
         status: "unread",//
         rating: "-",
         note: ""
@@ -19,6 +23,8 @@ export const booksList = [
         image: "./src/book-nosufoco.jpg",
         title: "No sufoco",
         author: "Chuck Palahniuk",
+        publisher:"Leya",
+        pages: "271",
         status: "read",
         rating: "7.5",
         note: "A trama de um protagonista egocêntrico, babaca e sexólatra. Essa obra aborda a vida com uma visão dura e necessária em tempos contemporâneos."
@@ -27,6 +33,8 @@ export const booksList = [
         image: "./src/book-cavaleirodossetereinos.jpg",
         title: "O Cavaleiro dos sete reinos",
         author: "George R.R. Martin",
+        publisher:"Leya",
+        pages: "413",
         status: "read",
         rating: "8.5",
         note: "Uma magnífica obra, ambientada no universo das Crônicas de Gelo e Fogo. A obra prende sua atenção em todo o tempo de leitura, abordando as lutas com uma narrativa impecável."
@@ -35,6 +43,8 @@ export const booksList = [
         image: "./src/book-1984.jpg",
         title: "1984",
         author: "George Orwell",
+        publisher:"Leya",
+        pages: "319",
         status: "read",
         rating: "9.2",
         note: "Sem dúvida alguma, uma obra que deve ler lida antes de morrer. Contribui para o desenvolvimento de pensamento crítico, além de levantar importantes questionamentos sobre nossa sociedade, levando em consideração a história distópica retratada na obra."
@@ -43,6 +53,8 @@ export const booksList = [
         image: "./src/book-freud.webp",
         title: "Psicologia aplicada de Freud",
         author: "-",
+        publisher:"-",
+        pages: "-",
         status: "read",
         rating: "7.2",
         note: "Apresenta influentes teses levantadas por Sigmund Freud com a Psicanálise."
@@ -51,6 +63,8 @@ export const booksList = [
         image: "./src/book-admiravelmundonovo.jpeg",
         title: "Admirável mundo novo",
         author: "Aldous Huxley",
+        publisher: "-",
+        pages: "-",
         status: "read",
         rating: "9.0",
         note: 'Ao lado de "1984", é uma das obras mais influentes de toda a história. Tal qual a obra de George Orwell, também apresenta um mundo distópico moralmente questionável, com um protagonista deslocado da sociedade e em busca da verdade.'
@@ -59,6 +73,8 @@ export const booksList = [
         image: "./src/book-maisespertoqueodiabo.jpg",
         title: "Mais esperto que o Diabo",
         author: "Napoleon Hil",
+        publisher:"-",
+        pages: "-",
         status: "read",
         rating: "6.9",
         note: "Essa obra argumenta sobre o impacto da fé e da positividade na vida de um indivíduo. Hill apresenta de forma excêntrica o conceito de medo na mente humana, em contraste com a fé individual."
@@ -67,6 +83,8 @@ export const booksList = [
         image: "./src/book-veronikadecidemorrer.webp",
         title: "Veronika decide morrer",
         author: "Paulo Coelho",
+        publisher:"-",
+        pages: "-",
         status: "read",
         rating: "8.0",
         note: ""
@@ -75,6 +93,8 @@ export const booksList = [
         image: "./src/book-arevolucaodosbichos.jpg",
         title: "A revolução dos bichos",
         author: "George Orwell",
+        publisher:"Leya",
+        pages: "319",
         status: "read",
         rating: "8.4",
         note: ""
@@ -83,6 +103,8 @@ export const booksList = [
         image: "./src/book-jogadorn1.jpg",
         title: "Jogador número 1",
         author: "Ernest Clins",
+        publisher:"Leya",
+        pages: "462",
         status: "read",
         rating: "8.0",
         note: ""
@@ -91,6 +113,8 @@ export const booksList = [
         image: "./src/book-guiadomochileirodasgalaxias.jpeg",
         title: "O Guia do mochileiro das galáxias",
         author: "Douglas Adams",
+        publisher:"Arqueiro",
+        pages: "",
         status: "read",
         rating: "9.2",
         note: "Uma das melhores obras - se não a melhor - de Ficção ja escritas. Douglas Adams aborda sua trama de forma irônica e divertida, com personagens carismáticos e uma história memorável."
